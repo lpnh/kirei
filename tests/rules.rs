7394116bf570947baa3e7c88494739f3f5567af7
lpnh <paniguel.lpnh@gmail.com>
2025-09-15 17:54:09 -0300

    test(whitespace): refactor to use fixture files

--- tests/rules.rs
+++ tests/rules.rs
@@ -13,11 +13,23 @@ fn html_blank_line() {
 }
 
 #[test]
+fn html_entity() {
+    format_and_snapshot!("rules/whitespace/html_entity.html");
+}
+
+#[test]
 fn tmpl_trailing_space() {
     format_and_snapshot!("rules/whitespace/tmpl_trailing_space.html");
 }
 
 #[test]
-fn entity() {
-    format_and_snapshot!("rules/whitespace/entity.html");
+fn tmpl_empty_comment() {
+    format_and_snapshot!("rules/whitespace/tmpl_empty_comment.html");
+}
+
+// === Delimiter Padding
+
+#[test]
+fn no_padding_expression() {
+    format_and_snapshot!("rules/delimiter_padding/no_padding_expression.html");
 }
